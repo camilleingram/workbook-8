48af62c479c00a104e6c0f709d6e2611077a9bae
Camille Ingram <camille_d_ingram@live.com>
2025-06-17 08:57:41 -0400

    Complete Mod 2 - Exercise 2

--- NorthwindTraders/src/main/java/com/pluralsight/App.java
+++ NorthwindTraders/src/main/java/com/pluralsight/App.java
@@ -17,19 +17,21 @@ public class App {
 
         Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind", username, password);
 
-        PreparedStatement preparedStatement = connection.prepareStatement("SELECT ProductId, ProductName, UnitPrice, " +
-                                                                          "UnitsInStock" +
-                                                                          " FROM " +
-                                                                          "Products");
+        PreparedStatement preparedStatement = connection.prepareStatement("SELECT ProductId, ProductName, UnitPrice UnitsInStock FROM Products");
 
         ResultSet resultSet = preparedStatement.executeQuery();
 
-       while(resultSet.next()) {
-           System.out.printf("Product Id: %d%n", resultSet.getInt("ProductID"));
-           System.out.printf("Name: %s%n", resultSet.getString("ProductName"));
-           System.out.printf("Price: %.2f%n", resultSet.getDouble("UnitPrice"));
-           System.out.printf("Stock: %d%n", resultSet.getInt("UnitsInStock"));
-           System.out.println("---------------");
-       }
+        while (resultSet.next()) {
+            System.out.printf("Product Id: %d%n", resultSet.getInt("ProductID"));
+            System.out.printf("Name: %s%n", resultSet.getString("ProductName"));
+            System.out.printf("Price: %.2f%n", resultSet.getDouble("UnitPrice"));
+            System.out.printf("Stock: %d%n", resultSet.getInt("UnitsInStock"));
+            System.out.println("---------------");
+        }
+
+        resultSet.close();
+        preparedStatement.close();
+        connection.close();
+
     }
 }

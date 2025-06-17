5e2b9eb16e852344f0bcca281c90d73f9a39ccec
Camille Ingram <camille_d_ingram@live.com>
2025-06-17 11:36:18 -0400

    Complete Mod 2 - Exercise 4

--- NorthwindTraders/src/main/java/com/pluralsight/App.java
+++ NorthwindTraders/src/main/java/com/pluralsight/App.java
@@ -17,84 +17,75 @@ public class App {
 
         Class.forName("com.mysql.cj.jdbc.Driver");
 
-        Connection connection = null;
-        PreparedStatement preparedStatement = null;
-        ResultSet resultSet = null;
-
-        try {
-
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind", username, password);
+        try(Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind", username, password);) {
 
             System.out.println("What do you want to do?");
             System.out.println("1) Display all products");
             System.out.println("2) Display all customers");
+            System.out.println("3) Display all categories");
             System.out.println("0) Exit");
             System.out.println("Select an option: ");
             int option = scanner.nextInt();
 
             switch (option) {
                 case 1:
-                    preparedStatement =
-                            connection.prepareStatement("SELECT ProductId, ProductName, UnitPrice, UnitsInStock FROM Products");
+                    System.out.println("Enter category id: ");
+                    int categoryId = scanner.nextInt();
+                    try(PreparedStatement preparedStatement = connection.prepareStatement("SELECT ProductId, ProductName, " +
+                            "UnitPrice, UnitsInStock FROM Products WHERE CategoryId = ?");) {
+                        preparedStatement.setInt(1, categoryId);
 
-                    resultSet = preparedStatement.executeQuery();
+                        try(ResultSet resultSet = preparedStatement.executeQuery();) {
+                            while (resultSet.next()) {
+                                System.out.printf("Product Id: %d%n", resultSet.getInt("ProductID"));
+                                System.out.printf("Name: %s%n", resultSet.getString("ProductName"));
+                                System.out.printf("Price: %.2f%n", resultSet.getDouble("UnitPrice"));
+                                System.out.printf("Stock: %d%n", resultSet.getInt("UnitsInStock"));
+                                System.out.println("---------------");
+                            }
+                        }
 
-                    while (resultSet.next()) {
-                        System.out.printf("Product Id: %d%n", resultSet.getInt("ProductID"));
-                        System.out.printf("Name: %s%n", resultSet.getString("ProductName"));
-                        System.out.printf("Price: %.2f%n", resultSet.getDouble("UnitPrice"));
-                        System.out.printf("Stock: %d%n", resultSet.getInt("UnitsInStock"));
-                        System.out.println("---------------");
                     }
                     break;
                 case 2:
-                    preparedStatement = connection.prepareStatement(
+
+                    try(PreparedStatement preparedStatement = connection.prepareStatement(
                             "SELECT ContactName, CompanyName, City, Country, Phone " +
                                     "FROM Customers " +
                                     "ORDER BY Country"
-                    );
-
-                    resultSet = preparedStatement.executeQuery();
-
-                    while (resultSet.next()) {
-                        System.out.printf("Name: %s%n", resultSet.getString("ContactName"));
-                        System.out.printf("Company Name: %s%n", resultSet.getString("CompanyName"));
-                        System.out.printf("City: %s%n", resultSet.getString("City"));
-                        System.out.printf("Country: %s%n", resultSet.getString("Country"));
-                        System.out.printf("Phone: %s%n", resultSet.getString("Phone"));
-                        System.out.println("---------------");
+                    );) {
+                        try(ResultSet resultSet = preparedStatement.executeQuery(); ) {
+                            while (resultSet.next()) {
+                                System.out.printf("Name: %s%n", resultSet.getString("ContactName"));
+                                System.out.printf("Company Name: %s%n", resultSet.getString("CompanyName"));
+                                System.out.printf("City: %s%n", resultSet.getString("City"));
+                                System.out.printf("Country: %s%n", resultSet.getString("Country"));
+                                System.out.printf("Phone: %s%n", resultSet.getString("Phone"));
+                                System.out.println("---------------");
+                            }
+                        }
                     }
+                    break;
+                case 3:
+                    try(PreparedStatement preparedStatement =
+                                connection.prepareStatement("SELECT CategoryID, CategoryName FROM Categories ORDER BY " +
+                                        "CategoryID");) {
+                        try(ResultSet resultSet = preparedStatement.executeQuery(); ) {
+                            while (resultSet.next()) {
+                                System.out.printf("ID: %d%n", resultSet.getInt("CategoryID"));
+                                System.out.printf("Name: %s%n", resultSet.getString("CategoryName"));
+                                System.out.println("---------------");
+                            }
+                        }
+                    }
+
                     break;
                 case 0:
                     System.out.println("Exiting...");
                     break;
             }
-        } catch (SQLException e) {
+        } catch (SQLException e){
             e.printStackTrace();
-        } finally {
-            if (resultSet != null) {
-                try {
-                    resultSet.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-
-            if (preparedStatement != null) {
-                try {
-                    preparedStatement.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-
-            if (connection != null) {
-                try {
-                    connection.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
         }
     }
 
